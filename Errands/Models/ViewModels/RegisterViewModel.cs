d2035c0fff7aef44ee45c9546609fd647b1d4c4d
Tanner Percival <percivaltanner@gmail.com>
2019-09-25 09:34:06 -0700

    setup viewmodels for login/register

--- Errands/Models/ViewModels/RegisterViewModel.cs
+++ Errands/Models/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,30 @@ namespace Errands.Models.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
