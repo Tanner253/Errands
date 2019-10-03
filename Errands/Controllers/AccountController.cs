6aa6e780eeea803152484e7d7a0cd180669b19ae
Tanner Percival <percivaltanner@gmail.com>
2019-10-03 10:13:12 -0700

    update applicationuser

--- Errands/Controllers/AccountController.cs
+++ Errands/Controllers/AccountController.cs
@@ -5,10 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Errands.Models.ViewModels;
 
 namespace Errands.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
@@ -19,12 +20,39 @@ namespace Errands.Controllers
             _signInManager = signInManager;
         }
 
-     /*  public IActionResult Register()
         [HttpGet]
+         public IActionResult Register()
         {
            return View();
         }
 
-        */
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    Email = rvm.Email,
+                    UserName = rvm.Email,
+                    NormalizedEmail = rvm.Email.ToLower(),
+                    FirstName = rvm.FirstName,
+                    LastName = rvm.LastName,
+                    Birthday = rvm.Birthday,
+
+                };
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError(string.Empty, "I'm sorry something went wrong. Please try again.");
+
+                }
+            }
+
+            ///setup logic for is eighteen or older claim
+        }
+
+        
     }
 }
